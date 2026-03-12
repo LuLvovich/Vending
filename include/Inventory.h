dd36c1f9c2074905a53221d91e4c4e6aae4bc0f1
LuLvovich <im727199@gmail.com>
2026-03-13 01:10:03 +0300

    Cmake-type repository

---
+++ include/Inventory.h
@@ -0,0 +1,46 @@
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Product-Liquid-Solid.h"
+#include <vector>
+#include <memory>
+
+
+class Inventory {
+private:
+    std::vector<std::unique_ptr<Product>> products;
+
+    static constexpr std::size_t MAX_UNIQUE_IDS = 60;
+
+public:
+    Inventory() = default;
+    ~Inventory() = default;
+
+
+    Inventory(const Inventory&) = delete;
+    Inventory& operator=(const Inventory&) = delete;
+
+
+    Inventory(Inventory&&) = delete;
+    Inventory& operator=(Inventory&&) = delete;
+
+
+    void AddLiquidProduct(int id, const std::string& name, int price, char size, int volume, bool sparkling);
+    void AddSolidProduct(int id, const std::string& name, int price, char size, int mass, bool careful);
+    bool AddProduct(std::unique_ptr<Product> product);
+
+
+    Product* FindById(int id) const;
+
+
+    bool GiveProduct(int id);
+
+
+    bool IdExists(int id) const;
+    size_t GetCount() const;
+    bool IsEmpty() const;
+    void DisplayAll(std::ostream& os) const;
+};
+
+#endif
